56ce50200aa0828afe66bbdb55015e1340101258
Andulla <andreas.wilke@puppetlabs.com>
2015-12-11 12:59:18 +0100

    updates for the files necessary to update ssh keys

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,53 +1,23 @@
-# == Class: pedemo
-#
-# Full description of class pedemo here.
-#
-# === Parameters
-#
-# Document parameters here.
-#
-# [*sample_parameter*]
-#   Explanation of what this parameter affects and what it defaults to.
-#   e.g. "Specify one or more upstream ntp servers as an array."
-#
-# === Variables
-#
-# Here you should define a list of variables that this module would require.
-#
-# [*sample_variable*]
-#   Explanation of how this variable affects the funtion of this class and if
-#   it has a default. e.g. "The parameter enc_ntp_servers must be set by the
-#   External Node Classifier as a comma separated list of hostnames." (Note,
-#   global variables should be avoided in favor of class parameters as
-#   of Puppet 2.6.)
-#
-# === Examples
-#
-#  class { 'pedemo':
-#    servers => [ 'pool.ntp.org', 'ntp.local.company.com' ],
-#  }
-#
-# === Authors
-#
-# Author Name <author@domain.com>
-#
-# === Copyright
-#
-# Copyright 2015 Your name here, unless otherwise noted.
-#
+# pedemo class
+
 class pedemo {
+  # My current SSH Key
+  $sshkey = 'My SSH key',
+  # Password for the local user in Windows
+  $password = 'MyPassword123',
   # Configurations for Linux Nodes
   if $::kernel == 'Linux' {
     file {'/root/.ssh':
       ensure => 'directory',
       owner  => 'root',
       group  => 'root',
+      mode   => '0700',
     } ->
 
     file { '/root/.ssh/authorized_keys':
       owner   => 'root',
       group   => 'root',
-      mode    => '0644',
+      mode    => '0640',
       content => template('pedemo/authorized_keys.erb'),
     }
   }
@@ -57,7 +27,7 @@ class pedemo {
       ensure   => 'present',
       comment  => 'Andreas Wilke TSE Demo Account',
       groups   => ['BUILTIN\Administrators'],
-      password => 'MyPassword123'
+      password => $password,
     }
   }
 
